cf6263b78b65cd60dee34490af35769d50c905cb
DuBing <tarvos21@live.com>
2016-09-25 20:53:48 +0800

    Exercise 34

---
+++ ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals
+print "The animal at 1: %s" % animals[1]
+print "The third(3rd) animal: %s" % animals[2]
+print "The first(1st) animal: %s" % animals[0]
+print "The animal at 3: %s" % animals[3]
+print "The fifth(5th) animal: %s" % animals[4]
+print "The animal at 2: %s" % animals[2]
+print "The sixth(6th) animal: %s" % animals[5]
+print "The animal at 4: %s" % animals[4]
