d7bac1125b7ee70c7c4c942f155f001f11b197ae
DuBing <tarvos21@live.com>
2016-09-25 20:33:13 +0800

    Exercise 46, 47

---
+++ ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'discription': 'My Project',
+        'author': 'DuBing',
+        'url': 'www.dubing.com',
+        'download_url': 'www.dubing.com/download',
+        'author_email': 'tarvos21@live.com',
+        'version': '1.0',
+        'install_requires': ['nose'],
+        'packages': ['ex47'],
+        'scripts': [],
+        'name': 'ex47'
+        }
+
+setup(**config)
